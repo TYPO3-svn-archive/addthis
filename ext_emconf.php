1b71ecc80182375a33a8e2abd9ca5ba6114e9c62
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-07 16:53:24 +0000

    Add Version to Ter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/addthis/trunk@43228 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,4 +1,15 @@
 <?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "addthis".
+#
+# Auto generated 07-02-2011 17:51
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'AddThis',
 	'description' => 'AddThis based on extbase',
@@ -22,8 +33,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'typo3' => '4.3.0',
-			'php' => '5.2.0',
+			'typo3' => '4.3.0-0.0.0',
+			'php' => '5.2.0-0.0.0',
 			'extbase' => '1.2.1',
 			'fluid' => '1.2.1',
 		),
@@ -32,5 +43,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:43:{s:25:"32x32 Icons (AddThis).gif";s:4:"cdba";s:25:"32x32 Icons (AddThis).png";s:4:"1a0e";s:21:"ext_conf_template.txt";s:4:"cda8";s:12:"ext_icon.gif";s:4:"5571";s:17:"ext_localconf.php";s:4:"187f";s:14:"ext_tables.php";s:4:"beaf";s:5:"tt.js";s:4:"dba1";s:25:"Classes/CodeGenerator.php";s:4:"4c39";s:18:"Classes/Config.php";s:4:"1b21";s:24:"Classes/ConfigReader.php";s:4:"42eb";s:18:"Classes/Wizard.php";s:4:"591c";s:40:"Classes/Controller/ButtonsController.php";s:4:"44bf";s:42:"Classes/ViewHelpers/RawValueViewHelper.php";s:4:"cb4e";s:27:"Configuration/languages.csv";s:4:"970e";s:26:"Configuration/services.csv";s:4:"eb2a";s:24:"Configuration/styles.csv";s:4:"714e";s:35:"Configuration/FlexForms/Buttons.xml";s:4:"1dc8";s:40:"Resources/Private/Language/locallang.xml";s:4:"5982";s:53:"Resources/Private/Language/locallang_csh_flexform.xml";s:4:"54e8";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"82eb";s:46:"Resources/Private/Templates/Buttons/index.html";s:4:"1e9b";s:34:"Resources/Public/Images/ce_wiz.gif";s:4:"3c93";s:50:"Resources/Public/Styles/32x32 Icons (AddThis).html";s:4:"33e0";s:49:"Resources/Public/Styles/32x32 Icons (AddThis).png";s:4:"3c82";s:41:"Resources/Public/Styles/Button Icons.html";s:4:"7085";s:40:"Resources/Public/Styles/Button Icons.png";s:4:"d9ea";s:35:"Resources/Public/Styles/Button.html";s:4:"6d31";s:34:"Resources/Public/Styles/Button.png";s:4:"a54f";s:53:"Resources/Public/Styles/Horizontal Share Counter.html";s:4:"9e56";s:52:"Resources/Public/Styles/Horizontal Share Counter.png";s:4:"f341";s:46:"Resources/Public/Styles/Share Count Ilike.html";s:4:"d7e5";s:45:"Resources/Public/Styles/Share Count Ilike.png";s:4:"ff75";s:52:"Resources/Public/Styles/Toolbox + Facebook Like.html";s:4:"4bac";s:51:"Resources/Public/Styles/Toolbox + Facebook Like.png";s:4:"c7e6";s:59:"Resources/Public/Styles/Toolbox Facebook Twitter Email.html";s:4:"4ef1";s:58:"Resources/Public/Styles/Toolbox Facebook Twitter Email.png";s:4:"4b2d";s:42:"Resources/Public/Styles/Toolbox Small.html";s:4:"7c2d";s:41:"Resources/Public/Styles/Toolbox Small.png";s:4:"c9b2";s:51:"Resources/Public/Styles/Vertical Share Counter.html";s:4:"71d5";s:50:"Resources/Public/Styles/Vertical Share Counter.png";s:4:"40ac";s:27:"Tests/CodeGeneratorTest.php";s:4:"a3d0";s:26:"Tests/ConfigReaderTest.php";s:4:"7a00";s:17:"Tests/phpunit.xml";s:4:"9443";}',
+	'suggests' => array(
+	),
 );
+
+?>
