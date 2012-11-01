86d6da05620ee88c649b29f8618d8a8bd6cd271b
stefan.masztalerz <stefan.masztalerz@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-01 11:04:18 +0000

    adds heise 2Click solution
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/addthis/tags/0.2.6@67573 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/ButtonsController.php
+++ Classes/Controller/ButtonsController.php
@@ -30,8 +30,21 @@ class Tx_Addthis_Controller_ButtonsController extends Tx_Extbase_MVC_Controller_
 		if(isset($this->settings['description']) && !empty($this->settings['description'])) {
 			$metatags->addDescription($this->settings['description']);
 		};
-		
+		if (FALSE !== strpos(base64_decode($this->settings['style']), 'socialshareprivacy')){
+			$this->include2ClickResources();
+		}
 	}
+
+	/**
+	 * @return void
+	 */
+	private function include2ClickResources() {
+		$GLOBALS ['TSFE']->getPageRenderer ()->addCssFile(t3lib_extMgm::extRelPath('addthis') . 'Resources/Public/Css/socialshareprivacy.css');
+		$GLOBALS ['TSFE']->getPageRenderer ()->addCssFile(t3lib_extMgm::extRelPath('addthis') . 'Resources/Public/Css/2Click.css');
+		$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterLibrary('addthis2click', t3lib_extMgm::extRelPath('addthis') . 'Resources/Public/Js/jquery.socialshareprivacy.js');
+		$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterLibrary('addthis2click_init', t3lib_extMgm::extRelPath('addthis') . 'Resources/Public/Js/socialshareprivacy.js');
+	}
+
 	/**
 	 * @return Tx_Addthis_Config
 	 */
