37a5ab4b8f570825966e0fadc7fd83aed9b0e13c
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-30 15:34:23 +0000

    new upload to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/addthis/trunk@68147 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "addthis".
-#
-# Auto generated 07-03-2011 15:10
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "addthis".
+ *
+ * Auto generated 30-11-2012 16:33
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'AddThis',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.2.3',
+	'version' => '0.2.9',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:42:{s:21:"ext_conf_template.txt";s:4:"b7a0";s:12:"ext_icon.gif";s:4:"5571";s:17:"ext_localconf.php";s:4:"187f";s:14:"ext_tables.php";s:4:"beaf";s:25:"Classes/CodeGenerator.php";s:4:"827c";s:18:"Classes/Config.php";s:4:"a0ae";s:24:"Classes/ConfigReader.php";s:4:"97b9";s:20:"Classes/Metatags.php";s:4:"72b0";s:18:"Classes/Wizard.php";s:4:"156f";s:40:"Classes/Controller/ButtonsController.php";s:4:"c7a1";s:42:"Classes/ViewHelpers/RawValueViewHelper.php";s:4:"cb4e";s:27:"Configuration/languages.csv";s:4:"970e";s:26:"Configuration/services.csv";s:4:"eb2a";s:24:"Configuration/styles.csv";s:4:"714e";s:35:"Configuration/FlexForms/Buttons.xml";s:4:"f645";s:40:"Resources/Private/Language/locallang.xml";s:4:"70f8";s:53:"Resources/Private/Language/locallang_csh_flexform.xml";s:4:"d1ed";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"95ac";s:46:"Resources/Private/Templates/Buttons/Index.html";s:4:"abd3";s:34:"Resources/Public/Images/ce_wiz.gif";s:4:"3c93";s:50:"Resources/Public/Styles/32x32 Icons (AddThis).html";s:4:"91d1";s:49:"Resources/Public/Styles/32x32 Icons (AddThis).png";s:4:"3c82";s:41:"Resources/Public/Styles/Button Icons.html";s:4:"c3ce";s:40:"Resources/Public/Styles/Button Icons.png";s:4:"d9ea";s:35:"Resources/Public/Styles/Button.html";s:4:"6d31";s:34:"Resources/Public/Styles/Button.png";s:4:"a54f";s:53:"Resources/Public/Styles/Horizontal Share Counter.html";s:4:"9e56";s:52:"Resources/Public/Styles/Horizontal Share Counter.png";s:4:"f341";s:46:"Resources/Public/Styles/Share Count Ilike.html";s:4:"36a1";s:45:"Resources/Public/Styles/Share Count Ilike.png";s:4:"ff75";s:52:"Resources/Public/Styles/Toolbox + Facebook Like.html";s:4:"8ec2";s:51:"Resources/Public/Styles/Toolbox + Facebook Like.png";s:4:"c7e6";s:59:"Resources/Public/Styles/Toolbox Facebook Twitter Email.html";s:4:"fbba";s:58:"Resources/Public/Styles/Toolbox Facebook Twitter Email.png";s:4:"4b2d";s:42:"Resources/Public/Styles/Toolbox Small.html";s:4:"3a76";s:41:"Resources/Public/Styles/Toolbox Small.png";s:4:"c9b2";s:51:"Resources/Public/Styles/Vertical Share Counter.html";s:4:"71d5";s:50:"Resources/Public/Styles/Vertical Share Counter.png";s:4:"40ac";s:27:"Tests/CodeGeneratorTest.php";s:4:"fedf";s:26:"Tests/ConfigReaderTest.php";s:4:"355e";s:17:"Tests/phpunit.xml";s:4:"9443";s:14:"doc/manual.sxw";s:4:"2ed3";}',
+	'_md5_values_when_last_written' => 'a:57:{s:21:"ext_conf_template.txt";s:4:"b7a0";s:12:"ext_icon.gif";s:4:"5571";s:17:"ext_localconf.php";s:4:"187f";s:14:"ext_tables.php";s:4:"beaf";s:25:"Classes/CodeGenerator.php";s:4:"61d9";s:18:"Classes/Config.php";s:4:"a0ae";s:24:"Classes/ConfigReader.php";s:4:"97b9";s:20:"Classes/Metatags.php";s:4:"7090";s:18:"Classes/Wizard.php";s:4:"156f";s:40:"Classes/Controller/ButtonsController.php";s:4:"0d45";s:42:"Classes/ViewHelpers/RawValueViewHelper.php";s:4:"cb4e";s:27:"Configuration/languages.csv";s:4:"970e";s:26:"Configuration/services.csv";s:4:"eb2a";s:24:"Configuration/styles.csv";s:4:"e1a9";s:35:"Configuration/FlexForms/Buttons.xml";s:4:"f645";s:40:"Resources/Private/Language/locallang.xml";s:4:"70f8";s:53:"Resources/Private/Language/locallang_csh_flexform.xml";s:4:"d1ed";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"7d14";s:46:"Resources/Private/Templates/Buttons/Index.html";s:4:"0323";s:31:"Resources/Public/Css/2Click.css";s:4:"c905";s:43:"Resources/Public/Css/socialshareprivacy.css";s:4:"91bd";s:34:"Resources/Public/Images/ce_wiz.gif";s:4:"3c93";s:49:"Resources/Public/Images/2Click/dummy_facebook.png";s:4:"e88a";s:52:"Resources/Public/Images/2Click/dummy_facebook_en.png";s:4:"5af6";s:46:"Resources/Public/Images/2Click/dummy_gplus.png";s:4:"ba29";s:50:"Resources/Public/Images/2Click/dummy_gplus_alt.png";s:4:"37e0";s:48:"Resources/Public/Images/2Click/dummy_twitter.png";s:4:"aed4";s:60:"Resources/Public/Images/2Click/socialshareprivacy_on_off.png";s:4:"498e";s:48:"Resources/Public/Js/jquery.socialshareprivacy.js";s:4:"7038";s:41:"Resources/Public/Js/socialshareprivacy.js";s:4:"13c1";s:35:"Resources/Public/Styles/2Click.html";s:4:"b3a1";s:34:"Resources/Public/Styles/2Click.png";s:4:"8505";s:50:"Resources/Public/Styles/32x32 Icons (AddThis).html";s:4:"91d1";s:49:"Resources/Public/Styles/32x32 Icons (AddThis).png";s:4:"3c82";s:41:"Resources/Public/Styles/Button Icons.html";s:4:"c3ce";s:40:"Resources/Public/Styles/Button Icons.png";s:4:"d9ea";s:35:"Resources/Public/Styles/Button.html";s:4:"6d31";s:34:"Resources/Public/Styles/Button.png";s:4:"a54f";s:53:"Resources/Public/Styles/Horizontal Share Counter.html";s:4:"9e56";s:52:"Resources/Public/Styles/Horizontal Share Counter.png";s:4:"f341";s:46:"Resources/Public/Styles/Share Count Ilike.html";s:4:"36a1";s:45:"Resources/Public/Styles/Share Count Ilike.png";s:4:"ff75";s:52:"Resources/Public/Styles/Toolbox + Facebook Like.html";s:4:"8ec2";s:51:"Resources/Public/Styles/Toolbox + Facebook Like.png";s:4:"c7e6";s:70:"Resources/Public/Styles/Toolbox Facebook Twitter Email GooglePlus.html";s:4:"94f8";s:69:"Resources/Public/Styles/Toolbox Facebook Twitter Email GooglePlus.png";s:4:"2de8";s:59:"Resources/Public/Styles/Toolbox Facebook Twitter Email.html";s:4:"fbba";s:58:"Resources/Public/Styles/Toolbox Facebook Twitter Email.png";s:4:"4b2d";s:42:"Resources/Public/Styles/Toolbox Small.html";s:4:"3a76";s:41:"Resources/Public/Styles/Toolbox Small.png";s:4:"c9b2";s:66:"Resources/Public/Styles/Twitter + Facebook Like + Google Plus.html";s:4:"ac81";s:51:"Resources/Public/Styles/Vertical Share Counter.html";s:4:"71d5";s:50:"Resources/Public/Styles/Vertical Share Counter.png";s:4:"40ac";s:27:"Tests/CodeGeneratorTest.php";s:4:"fedf";s:26:"Tests/ConfigReaderTest.php";s:4:"355e";s:17:"Tests/phpunit.xml";s:4:"9443";s:14:"doc/manual.sxw";s:4:"2ed3";}',
 	'suggests' => array(
 	),
 );
